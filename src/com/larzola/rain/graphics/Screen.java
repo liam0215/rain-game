df00bf31a3e57994da6c63c6bc474e5df719e4b5
BuildTools <unconfigured@null.spigotmc.org>
2017-05-08 20:38:55 -0400

    replaced sprite and made walking animation

--- src/com/larzola/rain/graphics/Screen.java
+++ src/com/larzola/rain/graphics/Screen.java
@@ -49,17 +49,21 @@ public class Screen {
 		}
 	}
 	
-	public void renderPlayer(int xp, int yp, Sprite sprite) {
+	public void renderPlayer(int xp, int yp, Sprite sprite, int flip) {
 		xp -= xOffset;
 		yp -= yOffset;
 		for(int y = 0; y < 32; y++) {
 			int ya = y + yp;
 			for(int x = 0; x < 32; x++) {
 				int xa = x + xp;
+				int xs = x;
+				if (flip == 1 || flip == 3) {
+					xs = 31 - x;
+				}
 				if(xa < -32 || xa >= width || ya < 0 || ya >= height) break;
 				if(xa < 0) xa = 0;
-				int col = sprite.pixels[x + y * 32];
-				if(col != 0xffff00ff) pixels[xa + ya * width] = col;
+				int col = sprite.pixels[xs + y * 32];
+				if(col != 0xffff00ff && col != 0xffffffff) pixels[xa + ya * width] = col;
 			}
 		}
 	}
